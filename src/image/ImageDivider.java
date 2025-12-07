52b98e88edeee31a604de0639dbad7319bfb4c3b
mfelig <mfelig@gmail.com>
2025-12-07 17:54:27 +0200

    FFS added SubImage and ImageDivider

---
+++ src/image/ImageDivider.java
@@ -0,0 +1,53 @@
+package image;
+
+import java.awt.*;
+import java.util.ArrayList;
+
+/**
+ * A public class of the package image.
+ * Divides a padded image into smaller sub-images of equal size.
+ * @author Eilam Soroka and Maayan Felig
+ */
+public class ImageDivider {
+    private final Image paddedImage;
+    private final int resolution;
+
+    /**
+     * Constructs an ImageDivider with the given padded image and block size.
+     * @param paddedImage the padded Image to be divided
+     * @param resolution the number of blocks in a row
+     */
+    public ImageDivider(Image paddedImage, int resolution) {
+        this.paddedImage = paddedImage;
+        this.resolution = resolution;
+    }
+
+    /**
+     * Divides the padded image into smaller sub-images of equal size.
+     * @return an ArrayList of SubImage objects
+     */
+    public ArrayList<SubImage> divide() {
+        int imgWidth = paddedImage.getWidth();
+        int imgHeight = paddedImage.getHeight();
+
+        int blockSize = imgWidth / resolution;
+        int numBlockRows = imgHeight / blockSize;
+
+        ArrayList<SubImage> subImages = new ArrayList<>();
+
+        for (int i = 0; i < numBlockRows; i++) {
+            for (int j = 0; j < blockSize; j++) {
+                Color[][] currentBlockPixels = new Color[blockSize][blockSize];
+
+                for(int y = 0; y < blockSize; y++) {
+                    for(int x = 0; x < blockSize; x++) {
+                        currentBlockPixels[y][x] = paddedImage.getPixel(x, y);
+                    }
+                }
+                subImages.add(new SubImage(currentBlockPixels));
+            }
+        }
+
+        return subImages;
+    }
+}
--- src/image/SubImage.java
+++ src/image/SubImage.java
@@ -2,20 +2,33 @@ package image;
 
 import java.awt.*;
 
+/**
+ * A public class of the package image.
+ * Represents a sub-image and calculates its brightness.
+ * @author Eilam Soroka and Maayan Felig
+ */
 public class SubImage {
 
     private double brightness;
     private Color[][] pixels;
     private static final int MAX_BRIGHTNESS = 255;
 
+    /**
+     * Constructs a SubImage with the given pixel array and calculates its brightness.
+     * @param pixels a 2D array of Color objects representing the pixels of the sub-image
+     */
     public SubImage(Color[][] pixels) {
-        this.brightness = calculateBrightness(pixels);
+        if(pixels == null || pixels.length == 0 || pixels[0].length == 0) {
+            throw new IllegalArgumentException("Pixel array cannot be null or empty");
+        }
+        //todo check what to do with invalid input
         this.pixels = pixels;
+        this.brightness = calculateBrightness();
     }
 
-    private double calculateBrightness(Color[][] pixels) {
+    private double calculateBrightness() {
         double total = 0.0;
-        for (Color[] row : pixels) {
+        for (Color[] row : this.pixels) {
             for (Color pixel : row) {
                 double greyPixel = pixel.getRed() * 0.2126 + pixel.getGreen() * 0.7152
                         + pixel.getBlue() * 0.0722;
@@ -25,7 +38,11 @@ public class SubImage {
         return total / (pixels.length * pixels[0].length * MAX_BRIGHTNESS);
     }
 
+    /**
+     * Returns the brightness of the sub-image.
+     * @return the brightness as a double value
+     */
     public double getBrightness() {
-        return brightness;
+        return this.brightness;
     }
 }
