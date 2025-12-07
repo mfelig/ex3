13eb363757a3efd90ce57cb85a1b4491c62fbec7
Eilam Soroka <eilamsoroka@gmail.com>
2025-12-07 12:39:12 +0200

    added the supplied mattierials and the calass SubImgCharMatcher.

---
+++ src/image_char_matching/SubImgCharMatcher.java
@@ -0,0 +1,14 @@
+package image_char_matching;
+
+public class SubImgCharMatcher {
+    public SubImgCharMatcher(char[] charset) {
+    }
+    public char getCharByImageBrightness(double brightness) {
+        return ' ';
+    }
+    public void addChar(char c) {
+    }
+    public void removeChar(char c) {
+    }
+
+}
