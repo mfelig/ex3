926cb8378c7e191b82a1979da1350305772e1866
mfelig <mfelig@gmail.com>
2025-12-10 13:08:02 +0200

    README file updated for image package and SubImgCharMatcher class

---
+++ src/SanityTests.java
@@ -0,0 +1,51 @@
+package image;
+
+import java.awt.Color;
+import java.io.IOException;
+
+public class SanityTests {
+    public static void main(String[] args) {
+//            Color[][] pixels = {
+//                    { new Color(0,0,0),     new Color(255,255,255) },
+//                    { new Color(255,0,0),   new Color(0,0,255)     }
+//            };
+
+        try{
+            Image img = new Image("src/examples/cat.jpeg");
+            // --- RUN TESTS ---
+            Image paddedImage = testPadder(img);
+              testDivider(paddedImage);
+//              testSubImageBrightness();
+        } catch (IOException e){
+            System.out.println("File not found");
+        }
+
+
+    }
+//    public class ImageSanityTest {
+
+    private static Image testPadder(Image img) {
+        System.out.println("Original width: " + img.getWidth());
+        System.out.println("Original height: " + img.getHeight());
+        Image paddedImage = new ImagePadder(img).getPaddedImage();
+
+        System.out.println("Padded width: " + paddedImage.getWidth());
+        System.out.println("Padded height: " + paddedImage.getHeight());
+        return paddedImage;
+    }
+    private static void testDivider(Image paddedImg) {
+        ImageDivider divider = new ImageDivider(paddedImg, 2);
+        var subs = divider.divide();
+
+        System.out.println("Number of subimages = " + subs.size());  // expect 4
+
+        for (int i = 0; i < subs.size(); i++) {
+            System.out.println("Subimage " + i + " brightness = " + subs.get(i).getBrightness());
+        }
+
+        System.out.println("---------------------------------------\n");
+    }
+
+
+//    }
+}
